923e82507c7d36f463227ba7cdd26a365caa4f74
Sedar Olmez <sedarolmez@Sedars-MacBook-Pro.local>
2017-11-26 09:18:39 +0000

    The script now produces a .txt file with the index of the most messaged command. 0, 1, 2, 3.

--- res/Twilio script/twilio_update.py
+++ res/Twilio script/twilio_update.py
@@ -14,6 +14,7 @@ flip_command =0
 client = TwilioRestClient(account_sid, auth_token)
 messages = client.messages.list()
 
+#client.messages.delete('SM9fb3501b1d569b2b3641298171d31363');
 
 for x in messages:
 	if(x.direction == 'inbound'):
@@ -25,14 +26,14 @@ for x in messages:
 		if(x.body == '!dark'):
  			dark_command += 1
 		if(x.body == '!flip'):
- 			invisible_command += 1
+ 			flip_command += 1
 
 array_values = [reverse_command, gravity_command, dark_command, flip_command]
 
 choice = array_values.index(max(array_values))
 
-with open("Output.txt", "w+") as text_file:
-    text_file.write(choice)
+with open("winning_poll.txt", "w+") as text_file:
+    text_file.write(str(choice))
 
 #data_frame = pd.DataFrame(array_values, index=['reverse', 'gravity', 'dark', 'invisible'],columns=['Points'])
 
