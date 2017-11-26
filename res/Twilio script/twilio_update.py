0e2cd07979c462dfc75209098b4c991545590c1a
Sedar Olmez <sedarolmez@Sedars-MacBook-Pro.local>
2017-11-26 11:22:01 +0000

    Updated, using threads.

--- res/Twilio script/twilio_update.py
+++ res/Twilio script/twilio_update.py
@@ -1,40 +1,47 @@
 from twilio.rest import TwilioRestClient
 import csv
 import pandas as pd
+import threading
 
 #DON'T TEXT YET!
 account_sid = ""
 auth_token = ""
-reverse_command = 0
-gravity_command = 0
-dark_command = 0
-flip_command =0
 
 
-client = TwilioRestClient(account_sid, auth_token)
-messages = client.messages.list()
 
-#client.messages.delete('SM9fb3501b1d569b2b3641298171d31363');
+def execute_function():
+	reverse_command = 0
+	gravity_command = 0
+	dark_command = 0
+	flip_command =0
 
-for x in messages:
-	if(x.direction == 'inbound'):
-		
-		if(x.body == '!reverse'):
-			reverse_command += 1
-		if(x.body == '!gravity'):
-			gravity_command += 1
-		if(x.body == '!dark'):
- 			dark_command += 1
-		if(x.body == '!flip'):
- 			flip_command += 1
+	threading.Timer(30.0, execute_function).start()
 
-array_values = [reverse_command, gravity_command, dark_command, flip_command]
+	client = TwilioRestClient(account_sid, auth_token)
+	messages = client.messages.list()
 
-choice = array_values.index(max(array_values))
+	#client.messages.delete('SM9fb3501b1d569b2b3641298171d31363');
 
-with open("winning_poll.txt", "w+") as text_file:
-    text_file.write(str(choice))
+	for x in messages:
+		if(x.direction == 'inbound'):
+			
+			if(x.body == '!reverse'):
+				reverse_command += 1
+			if(x.body == '!gravity'):
+				gravity_command += 1
+			if(x.body == '!dark'):
+	 			dark_command += 1
+			if(x.body == '!flip'):
+	 			flip_command += 1
 
+	array_values = [reverse_command, gravity_command, dark_command, flip_command]
+
+	choice = array_values.index(max(array_values))
+
+	with open("winning_poll.txt", "w+") as text_file:
+	    text_file.write(str(choice))
+
+execute_function()
 #data_frame = pd.DataFrame(array_values, index=['reverse', 'gravity', 'dark', 'invisible'],columns=['Points'])
 
 #data_frame.to_csv('game_poll.csv')
